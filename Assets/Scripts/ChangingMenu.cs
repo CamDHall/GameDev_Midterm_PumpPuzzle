16ef6b497b17b43c956d13bbbbf2d365d396d225
CamDHall <camerondylan.h@gmail.com>
2017-02-16 16:16:03 -0500

    ON/OFF Switch button

---
+++ Assets/Scripts/ChangingMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangingMenu : MonoBehaviour {
+    
+    // On/Off Switch
+    public Text onOff;
+    bool off = true;
+
+    public void OnOff()
+    {
+        if (off)
+        {
+            onOff.text = "ON";
+            off = false;
+        } else
+        {
+            onOff.text = "OFF";
+            off = true;
+        }
+    }
+}
